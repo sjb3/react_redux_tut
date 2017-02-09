4d35f221137904aabeaba82a3c8048b4b044a01a
Sung J Byun <sungjbyun@D-108-179-170-53.dhcp4.washington.edu>
2017-02-08 17:48:50 -0800

    adding state, cleaning codes with ES6

--- ReduxSimpleStarter-master/src/components/search_bar.js
+++ ReduxSimpleStarter-master/src/components/search_bar.js
@@ -7,9 +7,17 @@ import React, {Component} from 'react';
 // };
 
 class SearchBar extends Component{
-  render(){
-    return <input />;
+
+  constructor(props){
+    super(props);
+    this.state = {term: ''};
   }
+
+  render(){
+    // return <input onChange={this.onInputChange}/>; condensed version below
+    return <input onChange={event => console.log(event.target.value)} />;
+  }
+
 }
 
 export default SearchBar;
