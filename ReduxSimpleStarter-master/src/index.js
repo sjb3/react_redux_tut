f3e22342170428c72a962558d51af1e3e6d986c4
Sung J Byun <sungjbyun@D-128-95-62-245.dhcp4.washington.edu>
2017-02-10 15:15:00 -0800

    adapt error handing in case video is not loaded yet

--- ReduxSimpleStarter-master/src/index.js
+++ ReduxSimpleStarter-master/src/index.js
@@ -4,6 +4,7 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 import YTSearch from 'youtube-api-search';
 const API_KEY = 'AIzaSyCieOvskTOPX3SJ6XMrGxWPlou_NcQzWCc';
 
@@ -13,7 +14,7 @@ class App extends Component{
     super(props);
     this.state = {videos: []};
 
-    YTSearch({key: API_KEY, term: 'etro'},  (videos)=>{
+    YTSearch({key: API_KEY, term: 'pilates core'},  (videos)=>{
       this.setState({videos});
       // if the key and value have same name, u can put only one of them: this.setState = {videos: videos};
     });
@@ -24,6 +25,7 @@ class App extends Component{
     <div>
       <h2>PARTY!</h2>
         <SearchBar />
+        <VideoDetail video={this.state.videos[0]}/>
         {/*from parent to child*/}
         <VideoList videos={this.state.videos}/>
 
